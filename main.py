ad5d49791d5d0412b0607492645d07af92e65d9a
Yitau <yitau@foxmail.com>
2025-09-25 15:45:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def get_min_max_by_time(hour=None, minute=None):
 # 虚拟ip地址
 def fake_ip():
     # 随便找的国内IP段：223.64.0.0 - 223.117.255.255
-    return f"{223}.{random.randint(64, 117)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
+    return f"{202}.{random.randint(64, 117)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 
 # 账号脱敏
